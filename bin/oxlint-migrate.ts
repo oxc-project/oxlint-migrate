a2ff11037319b4847be42397433e4d9746865daa
Sysix <sysix@sysix-coding.de>
2025-02-14 17:49:14 +0100

    fix: allow default value for cli argument

--- bin/oxlint-migrate.ts
+++ bin/oxlint-migrate.ts
@@ -10,39 +10,39 @@ import packageJson from '../package.json' assert { type: 'json' };
 program
   .name('oxlint-migrate')
   .version(packageJson.version)
-  .argument('<eslint-config>', 'The path to the eslint v9 config file', '');
+  .argument('[eslint-config]', 'The path to the eslint v9 config file')
+  .action(async (filePath) => {
+    let cwd = process.cwd();
+
+    if (filePath === undefined) {
+      filePath = getAutodetectedEslintConfigName(cwd);
+
+      if (filePath === undefined) {
+        program.error(`could not autodetect eslint config file`);
+      }
+    } else {
+      filePath = path.join(cwd, filePath);
+    }
+
+    if (!existsSync(filePath)) {
+      // check for failed auto detection
+      program.error(`eslint config file not found: ${filePath}`);
+    } else {
+      const eslintConfigs = await import(filePath);
+
+      const oxlintConfig =
+        'default' in eslintConfigs
+          ? await main(eslintConfigs.default, console.warn)
+          : await main(eslintConfigs, console.warn);
+
+      const oxlintFilePath = path.join(cwd, '.oxlintrc.json');
+
+      if (existsSync(oxlintFilePath)) {
+        renameSync(oxlintFilePath, `${oxlintFilePath}.bak`);
+      }
+
+      writeFileSync(oxlintFilePath, JSON.stringify(oxlintConfig, null, 2));
+    }
+  });
 
 program.parse();
-
-let [filePath] = program.args;
-let cwd = process.cwd();
-
-if (filePath === '') {
-  filePath = getAutodetectedEslintConfigName(cwd) ?? '';
-
-  if (filePath === '') {
-    program.error(`could not autodetect eslint config file`);
-  }
-} else {
-  filePath = path.join(cwd, filePath);
-}
-
-if (!existsSync(filePath)) {
-  // check for failed auto detection
-  program.error(`eslint config file not found: ${filePath}`);
-} else {
-  const eslintConfigs = await import(filePath);
-
-  const oxlintConfig =
-    'default' in eslintConfigs
-      ? await main(eslintConfigs.default, console.warn)
-      : await main(eslintConfigs, console.warn);
-
-  const oxlintFilePath = path.join(cwd, '.oxlintrc.json');
-
-  if (existsSync(oxlintFilePath)) {
-    renameSync(oxlintFilePath, `${oxlintFilePath}.bak`);
-  }
-
-  writeFileSync(oxlintFilePath, JSON.stringify(oxlintConfig, null, 2));
-}
