c17fd20f3907cb9c197a30face3fb113473fe99f
Sysix <sysix@sysix-coding.de>
2025-06-16 19:12:58 +0200

    feat: error on json file because we only support flat configuration

--- bin/oxlint-migrate.ts
+++ bin/oxlint-migrate.ts
@@ -30,53 +30,60 @@ program
     'Include oxlint rules which are currently under development',
     false
   )
-  .action(async (filePath) => {
+  .action(async (filePath: string | undefined) => {
     const cliOptions = program.opts();
     const oxlintFilePath = path.join(cwd, cliOptions.outputFile);
 
     if (filePath === undefined) {
       filePath = getAutodetectedEslintConfigName(cwd);
-
-      if (filePath === undefined) {
-        program.error(`could not autodetect eslint config file`);
-      }
     } else {
       filePath = path.join(cwd, filePath);
     }
 
-    if (!existsSync(filePath)) {
-      // check for failed auto detection
-      program.error(`eslint config file not found: ${filePath}`);
-    } else {
-      // windows allows only file:// prefix to be imported, reported Error:
-      // Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs. Received protocol 'c:'
-      const eslintConfigs = await import(pathToFileURL(filePath).toString());
-
-      const options: Options = {
-        reporter: console.warn,
-        merge: !!cliOptions.merge,
-        withNursery: !!cliOptions.withNursery,
-      };
-
-      let config;
-      if (options.merge && existsSync(oxlintFilePath)) {
-        // we expect that is a right config file
-        config = JSON.parse(
-          readFileSync(oxlintFilePath, { encoding: 'utf8', flag: 'r' })
-        );
-      }
-
-      const oxlintConfig =
-        'default' in eslintConfigs
-          ? await main(eslintConfigs.default, config, options)
-          : await main(eslintConfigs, config, options);
-
-      if (existsSync(oxlintFilePath)) {
-        renameSync(oxlintFilePath, `${oxlintFilePath}.bak`);
-      }
-
-      writeFileSync(oxlintFilePath, JSON.stringify(oxlintConfig, null, 2));
+    if (filePath === undefined) {
+      program.error(`could not autodetect eslint config file`);
     }
+
+    // report when json file is found
+    if (filePath.endsWith('json')) {
+      program.error(
+        `json format is not supported. @oxlint/migrate only supports the eslint flat configuration`
+      );
+    }
+
+    // report when file does not exists
+    if (!existsSync(filePath)) {
+      program.error(`eslint config file not found: ${filePath}`);
+    }
+
+    // windows allows only file:// prefix to be imported, reported Error:
+    // Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs. Received protocol 'c:'
+    const eslintConfigs = await import(pathToFileURL(filePath).toString());
+
+    const options: Options = {
+      reporter: console.warn,
+      merge: !!cliOptions.merge,
+      withNursery: !!cliOptions.withNursery,
+    };
+
+    let config;
+    if (options.merge && existsSync(oxlintFilePath)) {
+      // we expect that is a right config file
+      config = JSON.parse(
+        readFileSync(oxlintFilePath, { encoding: 'utf8', flag: 'r' })
+      );
+    }
+
+    const oxlintConfig =
+      'default' in eslintConfigs
+        ? await main(eslintConfigs.default, config, options)
+        : await main(eslintConfigs, config, options);
+
+    if (existsSync(oxlintFilePath)) {
+      renameSync(oxlintFilePath, `${oxlintFilePath}.bak`);
+    }
+
+    writeFileSync(oxlintFilePath, JSON.stringify(oxlintConfig, null, 2));
   });
 
 program.parse();
