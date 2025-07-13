8970b5887bd53dcf2c82134149776c6954e817f7
Sysix <sysix@sysix-coding.de>
2025-07-13 15:53:34 +0200

    feat: replace disable directives comments

---
+++ src/replacer/index.test.ts
@@ -0,0 +1,120 @@
+import { describe, expect, it } from 'vitest';
+import { replaceRuleDirectiveComment } from './index.js';
+import { correctnessRules } from '../generated/rules.js';
+
+const eslintCommentsPrefixes = [
+  'eslint-disable',
+  'eslint-disable-line',
+  'eslint-disable-next-line',
+  'eslint-enable',
+];
+
+describe('replaceRuleDirectiveComment', () => {
+  describe('untouched comments', () => {
+    it('should keep non eslint comments', () => {
+      const comments = ['hello world', 'eslint-invalid-comment'];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(comment);
+      }
+    });
+
+    it('should keep eslint comments which disable / enable all rules', () => {
+      const comments = [
+        ...eslintCommentsPrefixes,
+        ...eslintCommentsPrefixes.map((prefix) => `${prefix} -- description`),
+        'eslint-disable -- no-debugger', // rule-name inside comment
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(comment);
+      }
+    });
+
+    it('should keep eslint comments for unsupported rules', () => {
+      const comments = [
+        ...eslintCommentsPrefixes.map((prefix) => `${prefix} unknown-rule`),
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} unknown-rule -- description`
+        ),
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(comment);
+      }
+    });
+
+    it('should keep eslint comments with multiple rules, where one of them is not supported', () => {
+      const comments = [
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} ${correctnessRules[0]}, unknown-rule`
+        ),
+        ...eslintCommentsPrefixes.map(
+          (prefix) =>
+            `${prefix} ${correctnessRules[0]}, unknown-rule -- description`
+        ),
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(comment);
+      }
+    });
+
+    it('should keep eslint comments with multiple rules, where the comma between is missing', () => {
+      const comments = [
+        // the comma is missing here, it will not count as an valid comment
+        // ______________________________________________________________________v
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} ${correctnessRules[0]} ${correctnessRules[1]}`
+        ),
+        ...eslintCommentsPrefixes.map(
+          (prefix) =>
+            `${prefix} ${correctnessRules[0]} ${correctnessRules[1]} -- description`
+        ),
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(
+          comment.replace('eslint', 'oxlint')
+        );
+      }
+    });
+  });
+
+  describe('touched comments', () => {
+    it('should replace single rule', () => {
+      const comments = [
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} ${correctnessRules[0]}`
+        ),
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} ${correctnessRules[0]} -- description`
+        ),
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(
+          comment.replace('eslint', 'oxlint')
+        );
+      }
+    });
+
+    it('should replace multiple rules', () => {
+      const comments = [
+        ...eslintCommentsPrefixes.map(
+          (prefix) => `${prefix} ${correctnessRules[0]}, ${correctnessRules[1]}`
+        ),
+        ...eslintCommentsPrefixes.map(
+          (prefix) =>
+            `${prefix} ${correctnessRules[0]}, ${correctnessRules[1]} -- description`
+        ),
+      ];
+
+      for (const comment of comments) {
+        expect(replaceRuleDirectiveComment(comment)).toBe(
+          comment.replace('eslint', 'oxlint')
+        );
+      }
+    });
+  });
+});
---
+++ src/replacer/index.ts
@@ -0,0 +1,25 @@
+import { globSync } from 'fast-glob';
+import { readFileSync } from 'node:fs';
+import { type Comment, parseSync } from 'oxc-parser';
+
+const getAllProjectFiles = (): string[] => {
+  return globSync(['**/*.{js,cjs,mjs,ts,cts,mts,vue,astro,svelte}'], {
+    absolute: true,
+  });
+};
+
+const getAstCommentsFromFilepath = (absoluteFilepath: string): Comment[] => {
+  const content = readFileSync(absoluteFilepath, 'utf-8');
+  const parserResult = parseSync(absoluteFilepath, content);
+
+  return parserResult.comments;
+};
+
+export const replaceRuleDirectiveComment = (comment: string): string => {
+  // this is not an eslint comment
+  if (!comment.includes('eslint')) {
+    return comment;
+  }
+
+  return comment;
+};
