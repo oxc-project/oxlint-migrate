8e9c1e9f726b7f32b5d283301c2e2edbfe00bed4
Sysix <sysix@sysix-coding.de>
2025-07-15 21:39:34 +0200

    test: `vue` and `regexp` plugins are no longer noicy

--- integration_test/utils.ts
+++ integration_test/utils.ts
@@ -18,8 +18,6 @@ export const getSnapshotResult = async (
       // filter out unsupported rules
       .filter((error) => !error.startsWith('unsupported rule: local/'))
       .filter((error) => !error.startsWith('unsupported rule: perfectionist/'))
-      .filter((error) => !error.startsWith('unsupported rule: vue/'))
-      .filter((error) => !error.startsWith('unsupported rule: regexp/'))
       .filter((error) => !error.startsWith('unsupported rule: toml/'))
       .filter((error) => !error.startsWith('unsupported rule: style/')),
   };
