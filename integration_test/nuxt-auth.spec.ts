9a2c8da147305c8b9313ecd8abb3910814ebd189
Sysix <sysix@sysix-coding.de>
2025-10-26 15:28:34 +0100

    refactor: simplify integration tests

--- integration_test/nuxt-auth.spec.ts
+++ integration_test/nuxt-auth.spec.ts
@@ -1,28 +1,5 @@
-import { expect, test } from 'vitest';
 // @ts-ignore
 import nuxt_auth_test from './projects/nuxt-auth.eslint.config.js';
-import { getSnapshotResult, getSnapShotMergeResult } from './utils.js';
+import { testProject } from './utils.js';
 
-test('nuxt-auth', async () => {
-  // https://github.com/antfu/eslint-config?tab=readme-ov-file#plugins-renaming
-  const result = await getSnapshotResult(nuxt_auth_test);
-  expect(result).toMatchSnapshot('nuxt-auth');
-});
-
-test('nuxt-auth --type-aware', async () => {
-  // https://github.com/antfu/eslint-config?tab=readme-ov-file#plugins-renaming
-  const result = await getSnapshotResult(nuxt_auth_test, undefined, {
-    typeAware: true,
-  });
-  expect(result).toMatchSnapshot('nuxt-auth--type-aware');
-});
-
-test('nuxt-auth merge', async () => {
-  const result = await getSnapShotMergeResult(nuxt_auth_test, {
-    categories: {
-      correctness: 'error',
-      perf: 'error',
-    },
-  });
-  expect(result).toMatchSnapshot('nuxt-auth--merge');
-});
+testProject('nuxt-auth', nuxt_auth_test);
