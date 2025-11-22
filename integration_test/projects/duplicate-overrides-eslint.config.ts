debd4899cf89339c2d7df7e6f0e408b24b78b5fb
Connor Shea <connor.james.shea@gmail.com>
2025-11-22 14:22:01 -0700

    Simplify example config/snapshot a lot.

--- integration_test/projects/duplicate-overrides-eslint.config.ts
+++ integration_test/projects/duplicate-overrides-eslint.config.ts
@@ -1,24 +1,26 @@
-import antfu from '@antfu/eslint-config';
+import { defineConfig } from 'eslint/config';
 
-export default await antfu(
+export default defineConfig([
   {
-    unocss: false,
-    vue: {
-      overrides: {
-        'vue/no-restricted-syntax': ['error'],
-      },
+    files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+  // These intentionally have identical rulesets to test merging when converting, this can happen in some complex
+  // configs, but it's easier to understand with an arbitrary example like this.
+  // As long as the plugins and rules and globals are all the same, these can
+  // be safely merged by merging the file globs.
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'arrow-body-style': 'error',
     },
   },
   {
+    files: ['**/*.mts', '**/*.cts'],
     rules: {
-      'node/prefer-global/process': 'off',
+      'arrow-body-style': 'error',
     },
   },
-  // Sort local files
-  {
-    files: ['locales/**.json'],
-    rules: {
-      'jsonc/sort-keys': 'error',
-    },
-  }
-);
+]);
