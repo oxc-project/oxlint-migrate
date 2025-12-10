263b53baee2db2b16fcb31c1023cbccfd7b916ef
Said Atrahouch <said.atrahouch@gmail.com>
2025-12-10 15:23:44 +0100

    feat(eslint-plugin-vitest): Update generate file using pnpm comand

--- src/generated/rules.ts
+++ src/generated/rules.ts
@@ -306,6 +306,7 @@ export const styleRules = [
   'vitest/no-hooks',
   'vitest/no-identical-title',
   'vitest/no-interpolation-in-snapshots',
+  'vitest/no-mocks-import',
   'vitest/no-restricted-jest-methods',
   'vitest/no-restricted-matchers',
   'vitest/no-test-prefixes',
