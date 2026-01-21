912b45904d96701dad400a0a200a6be6f6cb65dd
Connor Shea <connor.james.shea@gmail.com>
2026-01-20 19:33:25 -0700

    Add comment.

--- integration_test/projects/eslint-plugin-react-refresh.eslint.config.mjs
+++ integration_test/projects/eslint-plugin-react-refresh.eslint.config.mjs
@@ -1,3 +1,4 @@
 import reactRefresh from 'eslint-plugin-react-refresh';
 
+// This should result in an oxlint config with `react/only-export-components` enabled.
 export default [reactRefresh.configs.recommended];
