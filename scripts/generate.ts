bdd13c243106ba4bc9998b418278fc109a22a315
Connor Shea <connor.james.shea@gmail.com>
2026-01-24 13:40:55 -0700

    Split `pnpm generate` into two separate scripts so the import of the generated JSON file works.
    Otherwise, we get an error due to the import being resolved immediately upon execution of the file, before the JSON file exists (assuming it has been deleted).

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -2,45 +2,8 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { RulesGenerator } from './generator.js';
 import { traverseRules } from './traverse-rules.js';
-// import packageJson from '../package.json' with { type: 'json' };
-
-// --- Generate the vitest rules file ---
 
 const __dirname = new URL('.', import.meta.url).pathname;
-// `<repo>/scripts/generated/`
-const scriptsGenerateFolder = path.resolve(__dirname, `generated`);
-
-if (!fs.existsSync(scriptsGenerateFolder)) {
-  fs.mkdirSync(scriptsGenerateFolder);
-}
-
-// Generate the vitest-compatible-jest-rules.json file by pulling it from the oxc repository.
-// This keeps the two in sync.
-// Use the version of the package to determine which git ref to pull from.
-// const gitRef = `oxlint_v${packageJson.version}`;
-const gitRef = `main`; // temp so we can test that this works. DO NOT LEAVE THIS AS MAIN!
-
-const githubURL = `https://raw.githubusercontent.com/oxc-project/oxc/${gitRef}/crates/oxc_linter/data/vitest_compatible_jest_rules.json`;
-const response = await fetch(githubURL);
-
-if (!response.ok) {
-  throw new Error(
-    `Failed to fetch vitest-compatible-jest-rules.json: ${response.status} ${response.statusText}`
-  );
-}
-
-const vitestRules = await response.text();
-const vitestRulesPath = path.resolve(
-  scriptsGenerateFolder,
-  'vitest-compatible-jest-rules.json'
-);
-fs.writeFileSync(vitestRulesPath, vitestRules, 'utf-8');
-
-console.log(
-  'vitest-compatible-jest-rules.json copied successfully from the oxc repo.'
-);
-
-// --- Generate the rules files ---
 
 const result = traverseRules();
 const generator = new RulesGenerator(result);
