5982c7428b46fc554278664c5a6caef7f80a70c7
Sysix <sysix@sysix-coding.de>
2026-02-18 23:13:19 +0100

    fix: check nested `files` array size before skipping the config

---
+++ integration_test/projects/config-with-extends.eslint.config.mjs
@@ -0,0 +1,11 @@
+// eslint.config.js
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [tseslint.configs.recommended],
+    rules: { '@typescript-eslint/no-explicit-any': 'error' },
+  },
+]);
